204c5090a9992d765f9dd44d64dfd28ef1365e7e
womendexinshijie <womendexinshijie@sina.com>
2017-04-20 13:59:28 +0800

    tibia

---
+++ TestSourceTree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestSourceTree
+//
+//  Created by junhaoshen on 17/4/20.
+//  Copyright © 2017年 junhaoshen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestSourceTree/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  TestSourceTree
+//
+//  Created by junhaoshen on 17/4/20.
+//  Copyright © 2017年 junhaoshen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@" sadasc   sdad");
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
